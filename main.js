b89f6fa0263eaaf87ef31011a66beee4c16093f2
qqq <git@aaa.jp>
2021-06-08 18:20:34 +0900

    2

--- main.js
+++ main.js
@@ -137,33 +137,6 @@
         OrderValue -
         OrderValue_diff_between_1stchara_and_movechara;
 
-      // let tmp_movechara = objectCopy(this.current[movechara_nowPlace]);
-      // tmp_movechara.timeline_OrderValue = calculated_moved_OrderValue;
-
-      // let place_to_moved = -1;
-      // for (
-      //   let i = this.current.length - 1;
-      //   i > this.place_of_currentTimeline;
-      //   i--
-      // ) {
-      //   if (
-      //     calculated_moved_OrderValue >= this.current[i].timeline_OrderValue
-      //   ) {
-      //     place_to_moved = i;
-      //     break;
-      //   }
-      // }
-      // // splice 0 elm 1 elm 2
-      // // iter      0     1
-      // if (place_to_moved === -1) {
-      //   this.current.splice(
-      //     this.place_of_currentTimeline + 1,
-      //     0,
-      //     tmp_movechara
-      //   );
-      // } else {
-      //   this.current.splice(place_to_moved + 1, 0, tmp_movechara);
-      // }
       this.pushChara(id,calculated_moved_OrderValue)
       this.nextturn()
 
@@ -177,33 +150,33 @@
       }
     }
 
-    //古いコードのままになっている
-    move_stun(id, OrderValue) {
-      let movechara_nowPlace = this.placeToChara(id);
-      this.current[movechara_nowPlace].timeline_OrderValue += OrderValue;
-      let place_to_moved = () => {
-        if (movechara_nowPlace + 1 === this.current.length) {
-          return movechara_nowPlace + 1;
-        }
-        for (let i = movechara_nowPlace + 1; i < this.current.length; i++) {
-          if (
-            this.current[i].timeline_OrderValue >=
-            this.current[movechara_nowPlace].timeline_OrderValue
-          ) {
-            return i;
-          }
-        }
-        return i + 1;
-      };
-      this.current.splice(
-        place_to_moved(),
-        0,
-        this.current[movechara_nowPlace]
-      );
-      this.current.splice(movechara_nowPlace, 1);
+    // //古いコードのままになっている
+    // move_stun(id, OrderValue) {
+    //   let movechara_nowPlace = this.placeToChara(id);
+    //   this.current[movechara_nowPlace].timeline_OrderValue += OrderValue;
+    //   let place_to_moved = () => {
+    //     if (movechara_nowPlace + 1 === this.current.length) {
+    //       return movechara_nowPlace + 1;
+    //     }
+    //     for (let i = movechara_nowPlace + 1; i < this.current.length; i++) {
+    //       if (
+    //         this.current[i].timeline_OrderValue >=
+    //         this.current[movechara_nowPlace].timeline_OrderValue
+    //       ) {
+    //         return i;
+    //       }
+    //     }
+    //     return i + 1;
+    //   };
+    //   this.current.splice(
+    //     place_to_moved(),
+    //     0,
+    //     this.current[movechara_nowPlace]
+    //   );
+    //   this.current.splice(movechara_nowPlace, 1);
 
-      this.nextturn();
-    }
+    //   this.nextturn();
+    // }
 
     // 逆走の実装方法が思いつかない
     // move_ttk(ordervalue){
@@ -220,12 +193,7 @@
       this.pushChara( id,  initOrderValue);
     }
 
-    
-    pushChara(id,calculated_moved_OrderValue){
-      // let movechara_nowPlace = this.placeToChara(id);
-
-      let tmp_movechara = {id,timeline_OrderValue:calculated_moved_OrderValue}
-
+    place_to_moved(id,calculated_moved_OrderValue){
       let place_to_moved = -1;
       for (
         let i = this.current.length - 1;
@@ -239,18 +207,26 @@
           break;
         }
       }
+
+      let output;
       // splice 0 elm 1 elm 2
       // iter      0     1
       if (place_to_moved === -1) {
-        this.current.splice(
-          this.place_of_currentTimeline + 1,
-          0,
-          tmp_movechara
-        );
+        output = this.place_of_currentTimeline + 1;
       } else {
-        this.current.splice(place_to_moved + 1, 0, tmp_movechara);
+        output = place_to_moved + 1;
       }
 
+      return output;
+
+    }
+
+    
+    pushChara(id,calculated_moved_OrderValue){
+      let tmp_movechara = {id,timeline_OrderValue:calculated_moved_OrderValue}
+      let place_to_moved = this.place_to_moved(id,calculated_moved_OrderValue)
+
+      this.current.splice(place_to_moved, 0, tmp_movechara);
     }
 
     addChara(id, initOrderValue) {
