d5a577ab941aa01fc7f1e8c3b96e25cd28f016a4
y52en <y52en@example.com>
2021-07-07 01:49:01 +0900

    typescript

---
+++ bs-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  injectChanges: false,
+  files: ["./**/*.{html,htm,css,js}"],
+  watchOptions: { ignored: "node_modules" },
+  server: {
+    baseDir: "./public/",
+    // middleware: [[Function(anonymous)], [Function(anonymous)]],
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: "development",
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: path.resolve(__dirname, "src/index.ts"),
+
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
+        // TypeScript をコンパイルする
+        use: "ts-loader",
+      },
+    ],
+  },
+  // import 文で .ts ファイルを解決するため
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "./js/bundle.js",
+  },
+  devServer: {
+    // lazy: true,
+    // publicPath: "/public/",
+    contentBase: path.resolve(__dirname, "public"),
+  },
+};
